ff13769702ae1cd8269162cc478bd5578471eca1
Antisune101 <ewanbester72@gmail.com>
2024-03-19 09:18:54 +0200

    Created install script to swap out .nix files

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,136 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  security.polkit.enable = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Africa/Johannesburg";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_ZA.UTF-8";
+    LC_IDENTIFICATION = "en_ZA.UTF-8";
+    LC_MEASUREMENT = "en_ZA.UTF-8";
+    LC_MONETARY = "en_ZA.UTF-8";
+    LC_NAME = "en_ZA.UTF-8";
+    LC_NUMERIC = "en_ZA.UTF-8";
+    LC_PAPER = "en_ZA.UTF-8";
+    LC_TELEPHONE = "en_ZA.UTF-8";
+    LC_TIME = "en_ZA.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
+    xkb.layout = "us";
+    xkb.variant = "";
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    excludePackages = [ pkgs.xterm ];
+  };
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.antisune = {
+    isNormalUser = true;
+    description = "Ewan Bester";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "antisune";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    helix
+    nil
+    gparted
+
+    #Hyprland Apps
+    kitty
+    dunst
+    libnotify
+    rofi
+    waybar
+    swww
+
+    polkit-kde-agent
+    polkit_gnome
+
+
+    wireplumber
+    playerctl
+  ];
+
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
+
+
+
+  programs.zsh.enable = true;
+
+  programs.steam.enable = true;
+
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  xdg = {
+    portal.enable = true;
+    portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
+
+  #Audio configuration
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+
+  hardware.opengl.enable = true;
+
+  system.autoUpgrade.enable = true;
+
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
--- home.nix
+++ home.nix
@@ -1,1 +1,158 @@
-/home/antisune/.config/home-manager/home.nix
+{ config, pkgs, ... }:
+
+let 
+  home = builtins.getEnv "HOME";
+  dotfileDir = "${home}/.config/dotfiles";
+  install-script = (import "${dotfileDir}/modules/installscript.nix" {inherit pkgs; });
+  myEditor = "hx --config ~/.config/helix/config.toml";
+  myShellAliases = {
+    hypr = "${myEditor} ~/.config/hypr/hyprland.conf";
+
+    dots = "cd ~/.config/dotfiles";
+    home = "${myEditor} ~/.config/dotfiles/home.nix";
+    conf = "sudo ${myEditor} ~/.config/dotfiles/configuration.nix";
+    reshome = "home-manager switch";
+    resnix = "sudo nixos-rebuild switch";
+  };
+in {
+
+  imports = [
+    "${dotfileDir}/modules/waybar.nix"
+    "${dotfileDir}/modules/lf.nix"
+  ];
+
+  home.username = "antisune";
+  home.homeDirectory = "/home/antisune";
+
+  home.stateVersion = "23.11";
+
+
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 12;
+  };
+
+
+  nixpkgs.config.allowUnfree = true;
+  
+  home.packages = with pkgs; [
+    install-script
+    # Apps I use regularly
+    brave
+    discord
+    godot_4
+
+    # Utilities
+    xfce.thunar
+    mpv
+    font-awesome
+    nsxiv
+    
+    
+    # Extra apps that are cool
+    neofetch
+    htop
+    cava
+
+    # LSP
+    marksman
+    nodePackages.bash-language-server
+    
+  ];
+
+  xdg.mime.enable = true;
+
+  xdg.mimeApps =
+  let
+    imageViewer = "nsxiv.desktop";
+    myApps = {
+      "image/jpg" = imageViewer;
+      "image/jpeg" = imageViewer;
+      "image/png" = imageViewer;
+      "image/svg" = imageViewer;
+    };
+   in {
+    enable = true;
+    associations.added = myApps;
+    defaultApplications = myApps;
+  };
+
+  programs.kitty = {
+    enable = true;
+    theme = "Gruvbox Dark Hard";
+    font.name = "DejavuSansM Nerd Font Mono";
+    extraConfig = ''
+      confirm_os_window_close 0
+      background_opacity 0.8
+    '';
+  };
+
+
+  programs.bash = {
+    enable = true;
+    shellAliases = myShellAliases;
+  };
+
+  programs.zsh =  {
+    enable = true;
+    dotDir = ".config/zsh";
+    shellAliases = myShellAliases;
+
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    syntaxHighlighting.enable = true;
+    # The spaces in 'PS1 = ""' cost me the better part of a day
+    initExtra = ''
+      PS1="%F{yellow}[ %n ]%F{white}@%F{yellow}[ %~ ]%F{white}$ "
+    '';
+  };
+
+  programs.rofi = {
+    enable = true;
+    theme = "gruvbox-dark-hard";
+    extraConfig = {
+      show-icons = true;
+      display-drun = "Applications";
+      drun-display-format = "{name}";
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    settings.theme = "gruvbox_dark_hard";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Antisune101";
+    userEmail = "ewanbester72@gmail.com";
+  };
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "gruvbox-dark";
+    };
+  };
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name =  "Papirus";
+    };
+  };
+
+
+  home.sessionVariables = {
+    EDITOR = myEditor;
+    BROWSER = "brave";
+    PAGER = "bat";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
