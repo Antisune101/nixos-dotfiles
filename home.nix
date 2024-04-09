c25ae1ce733d01a7f05098f42f7b42ae412427ee
Antisune101 <ewanbester72@gmail.com>
2024-04-09 13:51:17 +0200

    Changed git defaultBranch to main

--- home.nix
+++ home.nix
@@ -146,6 +146,9 @@ in {
     enable = true;
     userName = "Antisune101";
     userEmail = "ewanbester72@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
   };
 
   programs.bat = {
