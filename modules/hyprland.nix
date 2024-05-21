4a81fc9331c7cb70e2dc388bd59ca49da1a8a8ea
Antisune101 <ewanbester72@gmail.com>
2024-05-21 10:19:45 +0200

    made calculator float

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -115,6 +115,7 @@
 
         windowrule = [
             "float, ^(thunar)$"
+            "float, ^(org.gnome.Calculator)$"
             # Way too much effort into making lf look nice Edit: It all went to waste (at least for now)
 
             # float,  class:^(kitty)$,title:^(lf)$
