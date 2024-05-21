e4e8d41a154937efa13a92b5531bfea6a144229b
Antisune101 <ewanbester72@gmail.com>
2024-05-21 10:24:28 +0200

    Fixed theming to use dark theme

--- home.nix
+++ home.nix
@@ -124,11 +124,17 @@ in {
     };
   };
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
   gtk = {
     enable = true;
     theme = {
-      name = "adw-gtk3-dark";
-      package = pkgs.adw-gtk3;
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
     };
     cursorTheme = {
       package = pkgs.bibata-cursors;
