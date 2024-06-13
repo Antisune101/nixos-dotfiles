25d9ad789a96b6069509ffa7e753876c27677279
Antisune101 <ewanbester72@gmail.com>
2024-06-13 08:43:55 +0200

    Added line that is required to enable stylix after the update

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   stylix = {
+    enable = true;
     image = ./wallpaper.png;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
