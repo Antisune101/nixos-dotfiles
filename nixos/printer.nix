1ea97b5e153ed4d6a14ae7daf26127aab71727bb
Antisune101 <ewanbester72@gmail.com>
2024-10-25 08:49:17 +0200

    Fixed printers, I think?

--- nixos/printer.nix
+++ nixos/printer.nix
@@ -4,11 +4,21 @@
   services = {
     printing = {
       enable = true;
-      drivers = [ pkgs.hplip ];
+      drivers = with pkgs; [ hplip cups-filters ];
+      browsing = true;
+      browsedConf = ''
+        BrowseDNSSDSubTypes _cups,_print
+        BrowseLocalProtocols all
+        BrowseRemoteProtocols all
+        CreateIPPPrinterQueues All
+
+        BrowseProtocols all
+      '';
     };
     avahi = {
       enable = true;
       nssmdns4 = true;
+      nssmdns6 = true;
       openFirewall = true;
     };
   };
