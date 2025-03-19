1c60b3a582dc5df07de660c7cbc98b9515ecff62
Antisune101 <ewanbseter72@gmail.com>
2025-03-19 07:48:17 +0200

    new hyprland config for new system

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -1,80 +1,110 @@
-{ inputs, pkgs, userSettings, ... }:
+{ pkgs, userSettings, inputs, ... }: {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
 
-{
-	programs.hyprland = {
-			enable = true;
-			xwayland.enable = true;
-			package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-			portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-	};
-		
-	home-manager.users.${userSettings.username}.wayland.windowManager.hyprland = {
-		enable = true;
-		settings = {
-			monitor = ",preferred,auto,auto";
-			"$terminal" = userSettings.terminal;
-			"$terminalFilemanager" = "${userSettings.terminal} yazi";
-			"$guiFilemanager" = "nautilus";
-			"$menu" = userSettings.hyprland.menu;
+  home-manager.users.${userSettings.username}.wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$terminal" = "kitty";
+      "$tuiFileManager" = "$terminal yazi";
+      "$guiFileManager" = "nautilus";
+      "$menu" = "rofi -show drun";
+      "$changeWallpaper" = "kitty yazi ~/Pictures/Wallpapers";
+      "$screenshotWindow" = "hyprshot -m window -o ~/Pictures/Screenshots";
+      "$screenshotArea" = "hyprshot -m region -o ~/Pictures/Screenshots";
+      
+      monitor = "DP-1, 1920x1080@180, 0x0, 1";
 
-			env = [
-				"XCURSOR_SIZE,24"
-				"HYPRCURSOR_SIZE,24"
-			];
+      exec-once = [
+        "swww-daemon"
+        "waybar"
+        "nm-applet"
+        "systemctl --user start hyprpolkitagent"
+      ];
 
-			general = {
-				gaps_in = 5;
-				gaps_out = 10;
-				border_size = 4;
-				resize_on_border = false;
-				allow_tearing = false;
-				layout = "master";
-			};
+      env = [
+        "XCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
+      ];
 
-			decoration = {
-				rounding = 0;
-				shadow = {
-					enabled = true;
-					range = 4;
-					render_power = 3;
-					# color = "rgba(1a1a1aee)";
-				};
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "master";
+      };
 
-				blur = {
-					enabled = true;
-					size = 3;
-					passes = 1;
-					vibrancy = 0.1696;
-				};
-			};
-			
-			dwindle = {
-				pseudotile = true;
-				preserve_split = true;
-			};
+      decoration = {
+        rounding = 0;
 
-			master = {
-				new_status = "master";
-			};
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          # color = "rgba(1a1a1aee)";
+        };
 
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+          vibrancy = 0.1696;
+        };
+      };
+      animations = {
+          enabled = true;
+          bezier = [
+            "easeOutQuint,0.23,1,0.32,1"
+            "easeInOutCubic,0.65,0.05,0.36,1"
+            "linear,0,0,1,1"
+            "almostLinear,0.5,0.5,0.75,1.0"
+            "quick,0.15,0,0.1,1"
+          ];
+          animation = [
+            "global, 1, 10, default"
+            "border, 1, 5.39, easeOutQuint"
+            "windows, 1, 4.79, easeOutQuint"
+            "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+            "windowsOut, 1, 1.49, linear, popin 87%"
+            "fadeIn, 1, 1.73, almostLinear"
+            "fadeOut, 1, 1.46, almostLinear"
+            "fade, 1, 3.03, quick"
+            "layers, 1, 3.81, easeOutQuint"
+            "layersIn, 1, 4, easeOutQuint, fade"
+            "layersOut, 1, 1.5, linear, fade"
+            "fadeLayersIn, 1, 1.79, almostLinear"
+            "fadeLayersOut, 1, 1.39, almostLinear"
+            "workspaces, 1, 1.94, almostLinear, fade"
+            "workspacesIn, 1, 1.21, almostLinear, fade"
+            "workspacesOut, 1, 1.94, almostLinear, fade"
+          ];
+      };
+      
+      master = {
+        new_status = "master";
+        mfact = 0.60;
+      };
 
-			misc = {
-				force_default_wallpaper = 0;
-				disable_hyprland_logo = true;
-				enable_swallow = true;
-				swallow_regex = "^(kitty)$";
-			};
+      misc = {
+        force_default_wallpaper = 0;
+        disable_hyprland_logo = true;
+        enable_swallow = true;
+        swallow_regex = "^(kitty)$";
+      };
 
-			animations.enabled = false;
-
-			input = {
-				kb_layout = "us";
-				kb_options = "compose:alt";
-				follow_mouse = 1;
-				sensitivity = 0;
-				touchpad.natural_scroll = false;
-			};
-
-		};
-	};
+      input = {
+        kb_layout = "us";
+        kb_options = "compose:alt";
+        follow_mouse = 1;
+        sensitivity = 0;
+        numlock_by_default = true;
+      };
+    };
+  };
 }
--- modules/hyprland/keybinds.nix
+++ modules/hyprland/keybinds.nix
@@ -1,94 +1,79 @@
-{ userSettings, ... }:
+{ pkgs, userSettings, ... }:
 
 {
-	home-manager.users.${userSettings.username}.wayland.windowManager.hyprland.settings = {
-		"$mainMod" = "SUPER";
-		bind = [
-		 # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-			"$mainMod, RETURN, exec, $terminal"
-			"$mainMod, Q, exec, $terminal"
-			"$mainMod, C, killactive,"
-			"$mainMod SHIFT, Q, exit,"
-			"$mainMod, E, exec, $terminalFilemanager"
-			"$mainMod SHIFT, E, exec, $guiFilemanager"
-			"$mainMod, V, togglefloating,"
-			"$mainMod, SPACE, togglefloating"
-			"$mainMod, R, exec, $menu"
-			"$mainMod, P, pseudo," # dwindle
-			"$mainMod, U, togglesplit," # dwindle
-			"$mainMod, F, fullscreen,"
+  home-manager.users.${userSettings.username}.wayland.windowManager.hyprland.settings = {
+    "$mainMod" = "SUPER";
 
-			# Screenshot
-			", PRINT, exec, hyprshot -m window -o ~/Pictures/Screenshots"
-			"shift, PRINT, exec, hyprshot -m region -o ~/Pictures/Screenshots"
+    bind = [
+      "$mainMod, RETURN, exec, $terminal"
+      "$mainMod SHIFT, RETURN, exec, $menu"
+      "$mainMod, C, killactive"
+      "$mainMod SHIFT, Q, exit"
+      "$mainMod, W, exec, $changeWallpaper"
+      # Launch file manager
+      # mainMod + E for tui, mainMod SHIFT E for gui
+      "$mainMod, E, exec, $tuiFileManager"
+      "$mainMod SHIFT, E, exec, $guiFileManager"
 
-			# Move focus with mainMod + vim keys
-			"$mainMod, h, movefocus, l"
-			"$mainMod, l, movefocus, r"
-			"$mainMod, k, movefocus, u"
-			"$mainMod, j, movefocus, d"
+      # Screenshot
+      "$mainMod, PRINT, exec, $screenshotWindow"
+      "$mainMod SHIFT, PRINT, exec, $screenshotArea"
 
-			# Move windows with mainMod + Shift + vim keys
-			"$mainMod SHIFT, h, movewindow, l"
-			"$mainMod SHIFT, l, movewindow, r"
-			"$mainMod SHIFT, k, movewindow, u"
-			"$mainMod SHIFT, j, movewindow, d"
+      
+      # Move focus with mainMod + vim keys 
+      "$mainMod, h, movefocus, l"
+      "$mainMod, l, movefocus, r"
+      "$mainMod, k, movefocus, u"
+      "$mainMod, j, movefocus, d"
 
+      # Move windows with mainMod Shift + vim keys
+      "$mainMod SHIFT, h, movewindow, l"
+      "$mainMod SHIFT, l, movewindow, r"
+      "$mainMod SHIFT, k, movewindow, u"
+      "$mainMod SHIFT, j, movewindow, d"
 
-			# Alt Tab through windows
-			"ALT, TAB, cyclenext"
-			"ALT SHIFT, Tab, cyclenext, prev"
+      # Switch workspaces with mainMod + [0-9]
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
 
-			# Switch workspaces with mainMod + [0-9]
-			"$mainMod, 1, workspace, 1"
-			"$mainMod, 2, workspace, 2"
-			"$mainMod, 3, workspace, 3"
-			"$mainMod, 4, workspace, 4"
-			"$mainMod, 5, workspace, 5"
-			"$mainMod, 6, workspace, 6"
-			"$mainMod, 7, workspace, 7"
-			"$mainMod, 8, workspace, 8"
-			"$mainMod, 9, workspace, 9"
-			"$mainMod, 0, workspace, 10"
+      # Move active window to a workspace with mainMod + SHIFT + [0-9]
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
 
-			# Move active window to a workspace with mainMod + SHIFT + [0-9]
-			"$mainMod SHIFT, 1, movetoworkspace, 1"
-			"$mainMod SHIFT, 2, movetoworkspace, 2"
-			"$mainMod SHIFT, 3, movetoworkspace, 3"
-			"$mainMod SHIFT, 4, movetoworkspace, 4"
-			"$mainMod SHIFT, 5, movetoworkspace, 5"
-			"$mainMod SHIFT, 6, movetoworkspace, 6"
-			"$mainMod SHIFT, 7, movetoworkspace, 7"
-			"$mainMod SHIFT, 8, movetoworkspace, 8"
-			"$mainMod SHIFT, 9, movetoworkspace, 9"
-			"$mainMod SHIFT, 0, movetoworkspace, 10"
+      # Scratchpads
+      # Music
+      "$mainMod, S, togglespecialworkspace, music"
+      "$mainMod SHIFT, S, movetoworkspace, special:music"
+      # Task Manager
+      "$mainMod, T, togglespecialworkspace, taskmanager"
+      "$mainMod SHIFT, T, movetoworkspace, special:taskmanager"
 
-			# Example special workspace (scratchpad)
-			"$mainMod, S, togglespecialworkspace, magic"
-			"$mainMod SHIFT, S, movetoworkspace, special:magic"
+      # Media keys
+      ", XF86AudioPlay, exec, playerctl --player=YoutubeMusic play-pause"
+      ", XF86AudioStop, exec, playerctl --player=firefox play-pause"
+      ", XF86AudioNext, exec, playerctl --player=YoutubeMusic next"
+      ", XF86AudioPrev, exec, playerctl --player=YoutubeMusic previous"
+    ];
 
-			# Scroll through existing workspaces with mainMod + scroll
-			"$mainMod, mouse_down, workspace, e+1"
-			"$mainMod, mouse_up, workspace, e-1"
-
-			# Media keys
-			", XF86AudioPlay, exec, playerctl --player=YoutubeMusic play-pause"
-			", XF86AudioStop, exec, playerctl --player=firefox play-pause"
-			", XF86AudioPrev, exec, playerctl previous"
-			", XF86AudioNext, exec, playerctl next"
-		];
-
-		binde = [
-			# Resize windows with mainMod "," "."
-			"$mainMod, COMMA, resizeactive, -10 0"
-			"$mainMod SHIFT, COMMA, resizeactive, 0 -10"
-			"$mainMod, PERIOD, resizeactive, 10 0"
-			"$mainMod SHIFT, PERIOD, resizeactive, 0 10"
-		];
-
-		bindm = [
-			"$mainMod, mouse:272, movewindow"
-			"$mainMod, mouse:273, resizewindow"
-		];
-	};
+    bindm = [
+      "$mainMod, mouse:272, movewindow"
+      "$mainMod, mouse:273, resizewindow"
+    ];
+  };
 }
