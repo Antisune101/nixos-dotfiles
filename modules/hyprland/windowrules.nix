fe4bae86bfb331ffc65fa57b72e6c3226c22a619
Antisune101 <ewanbseter72@gmail.com>
2025-03-20 07:06:23 +0200

    Made scratchpad for vekstop

--- modules/hyprland/windowrules.nix
+++ modules/hyprland/windowrules.nix
@@ -4,6 +4,7 @@
   home-manager.users.${userSettings.username}.wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "workspace special:music, initialTitle:(YouTube Music)"
+      "workspace special:discord, class:(vesktop)"
     ];
   };
 }
