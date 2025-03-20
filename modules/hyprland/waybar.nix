ce0592654895a4a3d2b61a52a064f2fbf6ccd287
Antisune101 <ewanbseter72@gmail.com>
2025-03-20 07:10:35 +0200

    some waybar config

--- modules/hyprland/waybar.nix
+++ modules/hyprland/waybar.nix
@@ -1,6 +1,18 @@
-{ pkgs, userSettings, ... }:
-
-{
+{ config, pkgs, userSettings, ... }:
+let 
+  stylixColors = config.lib.stylix.colors;
+  colors = {
+    main = "#${stylixColors.base00}";
+    red = "#${stylixColors.base01}";
+    orange = "#${stylixColors.base05}";
+    yellow = "#${stylixColors.base0A}";
+    green = "#${stylixColors.base0B}";
+    cyan = "#${stylixColors.base0C}";
+    blue = "#${stylixColors.base0D}";
+    purple = "#${stylixColors.base0E}";
+    brown = "#${stylixColors.base0F}";
+  };
+in {
   home-manager.users.${userSettings.username}.programs.waybar = {
     enable = true;
     settings = {
@@ -12,16 +24,16 @@
 
         modules-left = [
           "hyprland/workspaces"
-          "hyprland/window"
+          # "hyprland/window"
         ];
 
         modules-center = [
-          "clock"
         ];
 
         modules-right = [
           "mpris"
           "pulseaudio"
+          "clock"
           "tray"
         ];
 
@@ -36,7 +48,7 @@
         };
 
         mpris ={
-          format = "[ {title} | {artist} ] -> ({status})";
+          format = "[ {title} - {artist} ] -> {status}";
           title-len = 30;
           artist-len = 20;
           player = "YoutubeMusic";
@@ -45,9 +57,27 @@
         pulseaudio = {
           scroll-step = 2;
           max-volume = 100;
-          format = "Audio {volume}%";
-          format-source = "Mic {volume}%";
+          format = "[ Audio {volume}% ][ {format_source} ]";
+          format-source = "Mic ON ";
+          format-source-muted = "Mic OFF";
           on-click = "pavucontrol";
+        };
+
+        clock = {
+          format = "[ {:%H:%M} ]";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "left";
+            format = {
+              months = "<span color ='${colors.main}'><b>{}</b></span>";
+              days = "<span color ='${colors.main}'><b>{}</b></span>";
+              weeks = "<span color ='${colors.blue}'><b>W{}</b></span>";
+              weekdays = "<span color ='${colors.yellow}'><b>{}</b></span>";
+              today = "<span color ='${colors.red}'><b><u>{}</u></b></span>";
+            };
+          };
         };
 
         tray = {
@@ -55,5 +85,24 @@
         };
       };
     };
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: ${userSettings.font.name};
+      }
+
+      #clock {
+        color: ${colors.yellow};
+      }
+
+      #pulseaudio {
+        color: ${colors.red};
+      }
+
+      #mpris {
+        color: ${colors.blue};
+      }
+    '';
   };
 }
