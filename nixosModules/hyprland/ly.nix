9659783947290cede613203356fab476aaea8da0
Antisune101 <ewanbester72@gmail.com>
2025-07-10 11:48:55 +0200

    Added ly display manager to hyprland module

---
+++ nixosModules/hyprland/ly.nix
@@ -0,0 +1,13 @@
+{ config, lib, ... }:
+
+{
+  config = lib.mkIf config.hyprland.enable {
+    services.displayManager.ly = {
+      enable = true;
+      settings = {
+        animate = true;
+        animation = "doom";
+      };
+    };
+  };
+}
