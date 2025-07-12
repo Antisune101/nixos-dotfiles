c7ef03390117bfe7b59ee82f4993fc615bb76684
Antisune101 <ewanbester72@gmail.com>
2025-07-12 17:56:27 +0200

    Fixed indentation

--- nixosModules/nvf.nix
+++ nixosModules/nvf.nix
@@ -1,24 +1,29 @@
 { config, lib, inputs, ... }:
 
 {
-  options = {
-    nvf.enable = lib.mkEnableOption "Enables nvf for configuring neovim";
-  };
-
-  imports = [
-    inputs.nvf.nixosModules.default
-  ];
-
-  config = lib.mkIf config.nvf.enable {
-    programs.nvf = {
-      enable = true;
-      settings = {
-        vim = {
-          lineNumberMode = "relative";
-          languages.enableTreesitter = true;
-          options.shiftwidth = 4;
-        };
-      };
+    options = {
+        nvf.enable = lib.mkEnableOption "Enables nvf for configuring neovim";
     };
-  };
+
+    imports = [
+        inputs.nvf.nixosModules.default
+    ];
+
+    config = lib.mkIf config.nvf.enable {
+        programs.nvf = {
+            enable = true;
+            settings = {
+                vim = {
+                    lineNumberMode = "relative";
+                    options.shiftwidth = 4;
+                    telescope.enable = true;
+                    autocomplete.nvim-cmp.enable = true;
+                    languages = {
+                        enableTreesitter = true;
+
+                    };
+                };
+            };
+        };
+    };
 }
