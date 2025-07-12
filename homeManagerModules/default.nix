94826f04f819d542da36350dd72d6b865bebbd8b
Antisune101 <ewanbester72@gmail.com>
2025-07-12 18:05:23 +0200

    Fixed indentation

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -7,7 +7,7 @@
   ];
 
   # Module defaults
-  git.enable = lib.mkDefault true;
-  librewolf.enable = lib.mkDefault true;
-  kitty.enable = lib.mkDefault true;
+    git.enable = lib.mkDefault true;
+    librewolf.enable = lib.mkDefault true;
+    kitty.enable = lib.mkDefault true;
 }
