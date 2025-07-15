c5bbbc475d59c4f37934dc195975d68de31ac0b8
Antisune101 <ewanbester72@gmail.com>
2025-07-15 12:38:33 +0200

    Reordered alphabetically

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -4,9 +4,9 @@
     ./cliPrograms/git.nix
     ./cliPrograms/yazi.nix
     ./hyprland
-    ./guiPrograms/librewolf.nix
     ./guiPrograms/3d_printing.nix
     ./guiPrograms/kitty.nix
+    ./guiPrograms/librewolf.nix
     ./guiPrograms/yt-music.nix
   ];
 
@@ -14,8 +14,8 @@
     threeDPrinting.enable = lib.mkDefault true;
     btop.enable = lib.mkDefault true;
     git.enable = lib.mkDefault true;
-    librewolf.enable = lib.mkDefault true;
     kitty.enable = lib.mkDefault true;
+    librewolf.enable = lib.mkDefault true;
     yazi.enable = lib.mkDefault true;
     yt-music.enable = lib.mkDefault true;
 }
