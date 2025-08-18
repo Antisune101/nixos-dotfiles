ef06d75f092dea8f4337093fedd0103881344dfc
Antisune101 <ewanbester72@gmail.com>
2025-08-18 09:15:36 +0200

    Fixed swayosd not starting properly

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -17,7 +17,7 @@
                 exec-once = ( if config.hyprland.waybar.enable then [ "waybar" ] else [] )
                     ++ ( if config.hyprland.hyprpolkit.enable then [ "systemctl --user start hyprpolkitagent" ] else [] )
                     ++ ( if config.hyprland.swww.enable then [ "swww-daemon" ] else [] )
-                    ++ ( if config.hyprland.swayosd.enable then [ "systenctl --user restart swayosd" ] else [] );
+                    ++ ( if config.hyprland.swayosd.enable then [ "systemctl --user start swayosd" ] else [] );
 
                 monitor = globalConfig.hyprland.monitors;
 
