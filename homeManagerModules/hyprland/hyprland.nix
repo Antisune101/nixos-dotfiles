60e1f53492f803812e719c3649de491bc4282dec
Antisune101 <ewanbester72@gmail.com>
2025-08-19 07:35:29 +0200

    Decreased border size

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -29,7 +29,7 @@
                 general = {
                     gaps_in = 3;
                     gaps_out = 6;
-                    border_size = 3;
+                    border_size = 2;
                     allow_tearing = false;
                     layout = "master";
                 };
