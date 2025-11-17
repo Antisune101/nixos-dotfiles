b7983d096d70eec0dbaeb5542e1691b4307df0da
Antisune101 <ewanbester72@gmail.com>
2025-11-17 10:32:24 +0200

    Changed terminal background_transparency

--- homeManagerModules/guiPrograms/kitty.nix
+++ homeManagerModules/guiPrograms/kitty.nix
@@ -1,13 +1,16 @@
 { config, lib, ... }:
 
 {
-  options = {
-    kitty.enable = lib.mkEnableOption "Enables kitty terminal emulator";
-  };
-
-  config = lib.mkIf config.kitty.enable {
-    programs.kitty = {
-      enable = true;
+    options = {
+        kitty.enable = lib.mkEnableOption "Enables kitty terminal emulator";
     };
-  };
+
+    config = lib.mkIf config.kitty.enable {
+        programs.kitty = {
+            enable = true;
+            settings = {
+                background_opacity = lib.mkForce 0.8;
+            };
+        };
+    };
 }
