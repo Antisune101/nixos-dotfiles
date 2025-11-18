f67edad85d0f908cc419a6662cde4b49674f7c40
Antisune101 <ewanbester72@gmail.com>
2025-11-18 08:00:44 +0200

    Reformatted flake.nix to use proper indent size

--- flake.nix
+++ flake.nix
@@ -1,71 +1,70 @@
 {
-  description = "Nixos Configuration";
+    description = "Nixos Configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland.url = "github:hyprwm/hyprland";
-
-    hyprland-plugins = {
-        url = "github:hyprwm/hyprland-plugins";
-        inputs.hyprland.follows = "hyprland";
-    };
-
-    nvf = {
-      url = "github:notashelf/nvf/v0.8";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    stylix = {
-        url = "github:danth/stylix";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-  };
-
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; config.allowUnfreePredicate = _: true; };
-    lib = nixpkgs.lib;
-
-    userSettings = {
-        username = "antisune";
-        shellAliases = {
-            gl = "git log --oneline";
-            rebuild = "sudo nixos-rebuild switch --flake";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
-        font = {
-            name = "IosevkaTerm Nerd Font";
-            package = pkgs.nerd-fonts.iosevka-term;
+
+        hyprland.url = "github:hyprwm/hyprland";
+
+        hyprland-plugins = {
+            url = "github:hyprwm/hyprland-plugins";
+            inputs.hyprland.follows = "hyprland";
         };
-    };
-  in
-  {
-    nixosConfigurations = {
-      nixos-pc = lib.nixosSystem {
-        specialArgs = { inherit inputs; inherit system; inherit userSettings; };
-        modules = [
-          ./hosts/desktop/configuration.nix
-          ./nixosModules
-          ];
-      };
 
-      nixos-surface = lib.nixosSystem {
-        specialArgs = {inherit inputs; inherit system; inherit userSettings; };
-        modules = [
-          ./hosts/surface/configuration.nix
-          ./nixosModules
-        ];
-      };
+        nvf = {
+            url = "github:notashelf/nvf/v0.8";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        stylix = {
+            url = "github:danth/stylix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
     };
 
-    homeManagerModules.default = ./homeManagerModules;
-  };
+    outputs = { self, nixpkgs, ... }@inputs:
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; config.allowUnfreePredicate = _: true; };
+        lib = nixpkgs.lib;
+
+        userSettings = {
+            username = "antisune";
+            shellAliases = {
+                gl = "git log --oneline";
+                rebuild = "sudo nixos-rebuild switch --flake";
+            };
+            font = {
+                name = "IosevkaTerm Nerd Font";
+                package = pkgs.nerd-fonts.iosevka-term;
+            };
+        };
+    in {
+        nixosConfigurations = {
+            nixos-pc = lib.nixosSystem {
+                specialArgs = { inherit inputs; inherit system; inherit userSettings; };
+                modules = [
+                    ./hosts/desktop/configuration.nix
+                    ./nixosModules
+                ];
+            };
+
+            nixos-surface = lib.nixosSystem {
+                specialArgs = {inherit inputs; inherit system; inherit userSettings; };
+                modules = [
+                    ./hosts/surface/configuration.nix
+                    ./nixosModules
+                ];
+            };
+
+        };
+
+        homeManagerModules.default = ./homeManagerModules;
+    };
 }
