daa4063e881423af78b72e16a59198aa039c2d1f
Antisune101 <ewanbester72@gmail.com>
2025-12-01 07:58:52 +0200

    Refactored to use hyprland wallpaper variable

--- homeManagerModules/hyprland/customScripts/randwall.nix
+++ homeManagerModules/hyprland/customScripts/randwall.nix
@@ -1,10 +1,9 @@
 { config, lib, pkgs, userSettings, ... }:
 let 
-    dir = config.hyprland.customScripts.randwall.wallpaperDirectory;
     randwall = pkgs.writeShellScriptBin "randwall" ''
          #!/bin/sh
         CURRENT=$(basename "$(swww query | grep -oE '/[^ ]+\.(png|jpg|jpeg|gif|webp)')")
-        WALLS=$(find ${dir} -type f | grep -E '\.(png|jpg|jpeg|gif|webp)$')
+        WALLS=$(find ${config.hyprland.themedWallpaperDir} -type f | grep -E '\.(png|jpg|jpeg|gif|webp)$')
 
         if [ $(printf "%s\n" "$WALLS" | wc -l) -eq 1 ]; then
             NEW="$WALLS"
@@ -19,13 +18,7 @@ let
   '';
 in
 {
-    options.hyprland.customScripts.randwall = {
-        enable = lib.mkEnableOption "Enable random wallpaper utility";
-        wallpaperDirectory = lib.mkOption {
-            default = "${config.home.homeDirectory}/Pictures/Wallpapers";
-            description = "Directory where wallpapers are stored";
-        };
-    };
+    options.hyprland.customScripts.randwall.enable = lib.mkEnableOption "Enable random wallpaper utility";
 
     config = lib.mkIf config.hyprland.customScripts.randwall.enable {
         home.packages = with pkgs; [
