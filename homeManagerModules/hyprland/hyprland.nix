2919855983599311711eec4693f09802dd35c9e0
Antisune101 <ewanbester72@gmail.com>
2025-12-15 15:11:58 +0200

    Removed workspace and windowrules

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -82,17 +82,6 @@
                     sensitivity = 0;
                     numlock_by_default = true;
                 };
-
-                workspace = [
-                    "w[tv1], gapsout:0, gapsin:0"
-                    "f[1], gapsout:0, gapsin:0"
-                ];
-                windowrule = [
-                    "bordersize 0, floating:0, onworkspace:w[tv1]"
-                    "rounding 0, floating:0, onworkspace:w[tv1]"
-                    "bordersize 0, floating:0, onworkspace:f[1]"
-                    "rounding 0, floating:0, onworkspace:f[1]"
-                ];
             };
         };
     };
