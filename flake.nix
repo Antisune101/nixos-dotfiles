8ace48a1f7dc78c152724188d3e9fb789a17729e
Antisune101 <ewanbester72@gmail.com>
2026-01-26 12:22:10 +0200

    Changed nvf to main branch

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         };
 
         nvf = {
-            url = "github:notashelf/nvf/v0.8";
+            url = "github:notashelf/nvf";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
